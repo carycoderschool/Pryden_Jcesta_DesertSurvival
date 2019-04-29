42ad60f245af6923f4ee670dab675278d9979154
Peter <Peteryden@me.com>
2019-04-29 17:04:41 -0400

    Made the player controller simpler

--- TopDown Player Control/Assets/Player_Controller.cs
+++ TopDown Player Control/Assets/Player_Controller.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
+
     //Inspector Variables
-    float playerSpeed = 10.0F; //speed player moves
-    
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,28 +19,37 @@ public class Player_Controller : MonoBehaviour
     void Update()
     {
         MoveForward(); // Player Movement 
-       
+
     }
 
     void MoveForward()
     {
-        if (Input.GetKey("w"))//Press w key to move forward on the Y AXIS
+        float xMove = Input.GetAxis("Horizontal");
+        float yMove = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(xMove, yMove, 0.0f);
+
+        transform.Translate(movement * Time.deltaTime);
+    }
+
+}
+
+       /* if (Input.GetKey("w"))//Press w key to move forward on the Y AXIS
         {
             transform.Translate(0, playerSpeed * Time.deltaTime, 0);
         }
-        else if (Input.GetKey("s"))//Press s key to move downward on the Y AXIS
+        if (Input.GetKey("s"))//Press s key to move downward on the Y AXIS
         {
             transform.Translate(0, -playerSpeed * Time.deltaTime, 0);
         }
-        else if (Input.GetKey("a"))//Press a to move forward on the Y AXIS
+        if (Input.GetKey("a"))//Press a to move forward on the Y AXIS
         {
             transform.Translate(-playerSpeed * Time.deltaTime, 0, 0);
         }
-        else if (Input.GetKey("d"))//Press d key to move forward on the Y AXIS
+        if (Input.GetKey("d"))//Press d key to move forward on the Y AXIS
         {
             transform.Translate(playerSpeed * Time.deltaTime, 0, 0);
         }
+        */
 
-    }
-      
-}
+
