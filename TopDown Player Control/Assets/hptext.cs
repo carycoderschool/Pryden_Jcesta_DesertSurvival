dcefb7ab63ccfb1bf746923760458627f65b89b6
ナツオ   セスタ <49923596+KirboLirbo@users.noreply.github.com>
2019-05-13 16:22:39 -0400

    Health

---
+++ TopDown Player Control/Assets/hptext.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hptext : MonoBehaviour
+{ 
+    public Image heart1;
+    public Image heart2;
+    public Image heart3;
+    public Image heart4;
+    public Image heart5;
+    public Sprite HeartE;
+    public Sprite Heart1F;
+    public Sprite Heart2F;
+    public Sprite Heart3F;
+    public Sprite HeartFULL;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
