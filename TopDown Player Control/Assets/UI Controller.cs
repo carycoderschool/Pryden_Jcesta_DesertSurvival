8080d2a603a835186ba7cdc252e21bac50e15992
ナツオ   セスタ <49923596+KirboLirbo@users.noreply.github.com>
2019-05-13 16:57:38 -0400

    Hp ui
    ui

---
+++ TopDown Player Control/Assets/UI Controller.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Image heart1;
+    public Image heart2;
+    public Image heart3;
+    public Sprite full_heart;
+    public Sprite half_heart;
+    public Sprite empty_heart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Healthupdater(int HP)
+    {
+        if (HP == 1)
+        {
+            heart1.sprite = half_heart;
+            heart2.sprite = empty_heart;
+            heart3.sprite = empty_heart;
+        }
+
+        if (HP == 0)
+        {
+            heart1.sprite = empty_heart;
+            heart2.sprite = empty_heart;
+            heart3.sprite = empty_heart;
+        }
+
+        if (HP == 2)
+        {
+            heart1.sprite = full_heart;
+            heart2.sprite = empty_heart;
+            heart3.sprite = empty_heart;
+        }
+
+        if (HP == 3)
+        {
+            heart1.sprite = full_heart;
+            heart2.sprite = half_heart;
+            heart3.sprite = empty_heart;
+        }
+
+        if (HP == 4)
+        {
+            heart1.sprite = full_heart;
+            heart2.sprite = full_heart;
+            heart3.sprite = empty_heart;
+        }
+
+        if (HP == 5)
+        {
+            heart1.sprite = full_heart;
+            heart2.sprite = full_heart;
+            heart3.sprite = half_heart;
+        }
+
+        if (HP == 6)
+        {
+            heart1.sprite = full_heart;
+            heart2.sprite = full_heart;
+            heart3.sprite = full_heart;
+        }
+
+    }
+}
